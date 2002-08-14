396dddd3fb5f0662ab97ab677a9e5816c81d5896
Kenneth J. Pouncey <kjp1@users.sourceforge.net>
2002-08-14 22:25:17 +0000

    Fixed weird character causing this script no to run

--- scripts/testlooping.py
+++ scripts/testlooping.py
@@ -5,7 +5,7 @@ print "--------------- tn5250j test looping script start ------------"
 screen = _session.getScreen()
 
 while not _session.isStopMacroRequested():
-    screen.sendKeys("[pf5]")
-    time.sleep(5)
+    screen.sendKeys("[pf5]")
+    time.sleep(5)
 
 print "---------------- tn5250j test looping script end -------------"
