c8386e5ec045affa62af8d99ab81423204e72985
Kenneth J. Pouncey <kjp1@users.sourceforge.net>
2002-08-14 21:27:27 +0000

    Add splashscreen

---
+++ src/org/tn5250j/gui/TN5250jSplashScreen.java
@@ -0,0 +1,214 @@
+package org.tn5250j.gui;
+
+/**
+ * Title: TN5250SplashScreen.java
+ * Copyright:   Copyright (c) 2001
+ * Company:
+ * @author  Kenneth J. Pouncey
+ * @version 0.4
+ *
+ * Description:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA
+ *
+ */
+
+import javax.swing.JLabel;
+import java.awt.Frame;
+import java.awt.Window;
+import java.awt.Cursor;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.awt.MediaTracker;
+import java.awt.Canvas;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Color;
+
+/**
+ * Uses an Icon or the location of an image to create an application's introductory screen.
+ */
+public class TN5250jSplashScreen extends Canvas {
+
+   protected Window dialog = null;
+   protected Frame f = null;
+   protected Image image;
+	private Image offScreenBuffer;
+   private Graphics offScreenBufferGraphics;
+   private int steps;
+   private int progress;
+   private Object lock = new Object();
+
+   /**
+    * Creates a splash screen given the location of the image.
+    * The image location is the package path of the image and must be in
+    * the classpath. For example, if an image was located in
+    * /test/examples/image.gif, and the classpath specified contains /test,
+    * the constructor should be passed "/examples/image.gif".
+    */
+   public TN5250jSplashScreen(String image_location) {
+
+      // get the URL given the image location
+      URL url = getClass().getResource(image_location);
+
+      if (url == null) {
+         throw new IllegalArgumentException("Image could not be found: " +
+                                    image_location);
+      }
+      // convert URL to Image icon
+      ImageIcon image = new ImageIcon(url);
+      initialize(image);
+   }
+
+   /**
+    * Creates a splash screen given an Icon image.
+    */
+   public TN5250jSplashScreen(ImageIcon image) {
+      initialize(image);
+   }
+
+   /**
+    * Creates the Splash screen window and configures it.
+    */
+   protected void initialize(ImageIcon iimage) {
+
+      image = iimage.getImage();
+      // if no image, return
+      if (image == null) {
+         throw new IllegalArgumentException("Image specified is invalid.");
+      }
+//      System.out.println(" here in splash ");
+
+//		MediaTracker tracker = new MediaTracker(this);
+//		tracker.addImage(image,0);
+//
+//		try {
+//			tracker.waitForAll();
+//		}
+//		catch(Exception e) {
+//         System.out.println(e.getMessage());
+//		}
+
+      // create dialog window
+      f = new Frame();
+      dialog = new Window(f);
+      dialog.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+		Dimension s = new Dimension(image.getWidth(this) + 2,
+			image.getHeight(this) + 2);
+		dialog.setSize(s);
+
+      dialog.add(this);
+      dialog.validate();
+      dialog.pack();
+
+      // position splash screen
+      Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+      int x = (screen.width - s.width)/2;
+      if (x < 0) {
+         x = 0;
+      }
+
+      int y = (screen.height - s.height)/2;
+      if (y < 0) {
+         y = 0;
+      }
+
+      dialog.setLocation(x, y);
+//      try {
+//         Thread.sleep(1000);
+//      }
+//      catch (InterruptedException i) {
+//         System.out.println(" here " + i.getMessage()  );
+//
+//      }
+
+   }
+
+   public void setSteps(int step) {
+      steps = step;
+   }
+
+   public synchronized void updateProgress(int prog) {
+
+		progress = prog;
+		repaint();
+
+		// wait for it to be painted to ensure progress is updated
+		// continuously
+//		try {
+//			wait();
+//		}
+//		catch(InterruptedException ie) {
+//         System.out.println(" updateProgress " + ie.getMessage()  );
+//		}
+   }
+
+	public void update(Graphics g) {
+		paint(g);
+	}
+
+	public synchronized void paint(Graphics g) {
+		Dimension size = getSize();
+//      System.out.println(" here in paint ");
+		if(offScreenBuffer == null) {
+			offScreenBuffer = createImage(size.width,size.height);
+			offScreenBufferGraphics = offScreenBuffer.getGraphics();
+		}
+
+		offScreenBufferGraphics.setColor(Color.black);
+		offScreenBufferGraphics.drawRect(0,0,size.width - 1,size.height - 1);
+
+		offScreenBufferGraphics.drawImage(image,1,1,this);
+
+      offScreenBufferGraphics.draw3DRect(4,image.getHeight(this) - 16,
+                              image.getWidth(this) - 8,15,true);
+
+      offScreenBufferGraphics.setColor(new Color(206,206,229));
+      offScreenBufferGraphics.fillRect(5,image.getHeight(this) - 15,
+                              ((image.getWidth(this) - 10) / steps) * progress,14);
+		g.drawImage(offScreenBuffer,0,0,this);
+
+//      System.out.println(" here after paint ");
+	}
+
+   /**
+    * This method will show or hide the splash screen.  Once the splash
+    * screen is hidden, the splash screen window will be disposed. This means
+    * the splash screen cannot become visible again.
+    */
+   public void setVisible(boolean show) {
+      if (show == true && dialog != null && f != null && !dialog.isVisible()) {
+         dialog.setVisible(true);
+      }
+      else {
+
+         if (dialog != null) {
+            updateProgress(steps + 1);
+            dialog.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+            dialog.dispose();
+         }
+         if (f != null) {
+            f.dispose();
+         }
+         dialog = null;
+         f = null;
+      }
+   }
+
+}
