9a8821177c454117b897039f8f8595f3b428eccb
Kenneth J. Pouncey <kjp1@users.sourceforge.net>
2003-02-18 19:05:22 +0000

    added new actions key handling

--- src/org/tn5250j/keyboard/DefaultKeyboardHandler.java
+++ src/org/tn5250j/keyboard/DefaultKeyboardHandler.java
@@ -33,7 +33,9 @@ import java.util.Hashtable;
 import java.util.StringTokenizer;
 import javax.swing.Action;
 import javax.swing.AbstractAction;
+
 import org.tn5250j.Session;
+import org.tn5250j.keyboard.actions.*;
 
 /**
  * The default keyboard input handler.
@@ -46,7 +48,6 @@ public class DefaultKeyboardHandler extends KeyboardHandler {
     */
    public DefaultKeyboardHandler(Session session) {
       super(session);
-
    }
 
    public boolean isKeyStrokeDefined(String accelKey) {
@@ -137,259 +138,21 @@ public class DefaultKeyboardHandler extends KeyboardHandler {
 
       KeyStroke ks;
 
-      Action newSession = new AbstractAction(MNEMONIC_OPEN_NEW) {
-            public void actionPerformed(ActionEvent e) {
-               session.startNewSession();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_OPEN_NEW)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_N,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_OPEN_NEW);
-      }
-
-      getInputMap().put(ks,MNEMONIC_OPEN_NEW);
-      getActionMap().put(MNEMONIC_OPEN_NEW,newSession );
-
-      Action chgSession = new AbstractAction(MNEMONIC_TOGGLE_CONNECTION) {
-            public void actionPerformed(ActionEvent e) {
-               session.changeConnection();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_TOGGLE_CONNECTION)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_X,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_TOGGLE_CONNECTION);
-      }
-
-      getInputMap().put(ks,MNEMONIC_TOGGLE_CONNECTION);
-      getActionMap().put(MNEMONIC_TOGGLE_CONNECTION,chgSession );
-
-      Action nxtSession = new AbstractAction(MNEMONIC_JUMP_NEXT) {
-            public void actionPerformed(ActionEvent e) {
-               session.nextSession();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_JUMP_NEXT)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_UP,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_JUMP_NEXT);
-      }
-
-      getInputMap().put(ks,MNEMONIC_JUMP_NEXT);
-      getActionMap().put(MNEMONIC_JUMP_NEXT,nxtSession );
-
-      // check for alternate
-      if (keyMap.isKeyStrokeDefined(MNEMONIC_JUMP_NEXT + ".alt2")) {
-         ks = keyMap.getKeyStroke(MNEMONIC_JUMP_NEXT + ".alt2");
-         getInputMap().put(ks,MNEMONIC_JUMP_NEXT);
-         getActionMap().put(MNEMONIC_JUMP_NEXT,nxtSession );
-      }
-
-
-      Action prevSession = new AbstractAction(MNEMONIC_JUMP_PREV) {
-            public void actionPerformed(ActionEvent e) {
-               session.prevSession();
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_JUMP_PREV)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_DOWN,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_JUMP_PREV);
-      }
-      getInputMap().put(ks,MNEMONIC_JUMP_PREV);
-      getActionMap().put(MNEMONIC_JUMP_PREV,prevSession );
-
-      // check for alternate
-      if (keyMap.isKeyStrokeDefined(MNEMONIC_JUMP_PREV + ".alt2")) {
-         ks = keyMap.getKeyStroke(MNEMONIC_JUMP_PREV + ".alt2");
-         getInputMap().put(ks,MNEMONIC_JUMP_PREV);
-         getActionMap().put(MNEMONIC_JUMP_PREV,prevSession );
-      }
-
-      Action hotSpots = new AbstractAction(MNEMONIC_HOTSPOTS) {
-            public void actionPerformed(ActionEvent e) {
-               screen.toggleHotSpots();
-               System.out.println(MNEMONIC_HOTSPOTS);
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_HOTSPOTS)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_S,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_HOTSPOTS);
-      }
-
-      getInputMap().put(ks,MNEMONIC_HOTSPOTS);
-      getActionMap().put(MNEMONIC_HOTSPOTS,hotSpots );
-
-      Action gui = new AbstractAction(MNEMONIC_GUI) {
-            public void actionPerformed(ActionEvent e) {
-               screen.toggleGUIInterface();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_GUI)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_G,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_GUI);
-      }
-      getInputMap().put(ks,MNEMONIC_GUI);
-      getActionMap().put(MNEMONIC_GUI,gui );
-
-      Action msg = new AbstractAction(MNEMONIC_DISP_MESSAGES) {
-            public void actionPerformed(ActionEvent e) {
-               getVT().systemRequest('4');
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_DISP_MESSAGES)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_M,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_DISP_MESSAGES);
-      }
-      getInputMap().put(ks,MNEMONIC_DISP_MESSAGES);
-      getActionMap().put(MNEMONIC_DISP_MESSAGES,msg );
-
-      Action attr = new AbstractAction(MNEMONIC_DISP_ATTRIBUTES) {
-            public void actionPerformed(ActionEvent e) {
-               session.doAttributes();
-
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_DISP_ATTRIBUTES)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_D,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_DISP_ATTRIBUTES);
-      }
-      getInputMap().put(ks,MNEMONIC_DISP_ATTRIBUTES);
-      getActionMap().put(MNEMONIC_DISP_ATTRIBUTES,attr );
-
-      Action print = new AbstractAction(MNEMONIC_PRINT_SCREEN) {
-            public void actionPerformed(ActionEvent e) {
-               session.printMe();
-
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_PRINT_SCREEN)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_P,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_PRINT_SCREEN);
-      }
-      getInputMap().put(ks,MNEMONIC_PRINT_SCREEN);
-      getActionMap().put(MNEMONIC_PRINT_SCREEN,print );
-
-      Action cursor = new AbstractAction(MNEMONIC_CURSOR) {
-            public void actionPerformed(ActionEvent e) {
-               screen.crossHair();
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_CURSOR)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_L,KeyEvent.ALT_MASK,false);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_CURSOR);
-      }
-      getInputMap().put(ks,MNEMONIC_CURSOR);
-      getActionMap().put(MNEMONIC_CURSOR,cursor );
-
-      Action debug = new AbstractAction(MNEMONIC_DEBUG) {
-            public void actionPerformed(ActionEvent e) {
-               session.toggleDebug();
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_DEBUG)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_O,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_DEBUG);
-      }
-      getInputMap().put(ks,MNEMONIC_DEBUG);
-      getActionMap().put(MNEMONIC_DEBUG,debug );
-
-      Action close = new AbstractAction(MNEMONIC_CLOSE) {
-            public void actionPerformed(ActionEvent e) {
-               session.closeSession();
-            }
-        };
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_CLOSE)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_Q,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_CLOSE);
-      }
-      getInputMap().put(ks,MNEMONIC_CLOSE);
-      getActionMap().put(MNEMONIC_CLOSE,close );
-
-      Action transfer = new AbstractAction(MNEMONIC_FILE_TRANSFER) {
-            public void actionPerformed(ActionEvent e) {
-               session.doMeTransfer();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_FILE_TRANSFER)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_T,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_FILE_TRANSFER);
-      }
-      getInputMap().put(ks,MNEMONIC_FILE_TRANSFER);
-      getActionMap().put(MNEMONIC_FILE_TRANSFER,transfer );
-
-      Action e_mail = new AbstractAction(MNEMONIC_E_MAIL) {
-            public void actionPerformed(ActionEvent e) {
-               session.sendScreenEMail();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_E_MAIL)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_E,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_E_MAIL);
-      }
-      getInputMap().put(ks,MNEMONIC_E_MAIL);
-      getActionMap().put(MNEMONIC_E_MAIL,e_mail );
-
-      Action runScript = new AbstractAction(MNEMONIC_RUN_SCRIPT) {
-            public void actionPerformed(ActionEvent e) {
-               session.runScript();
-            }
-        };
-
-      if (!keyMap.isKeyStrokeDefined(MNEMONIC_RUN_SCRIPT)) {
-         ks = KeyStroke.getKeyStroke(KeyEvent.VK_R,KeyEvent.ALT_MASK);
-      }
-      else {
-         ks = keyMap.getKeyStroke(MNEMONIC_RUN_SCRIPT);
-      }
-      getInputMap().put(ks,MNEMONIC_RUN_SCRIPT);
-      getActionMap().put(MNEMONIC_RUN_SCRIPT,runScript );
-
-//      Action spclDump = new AbstractAction("special dump") {
-//            public void actionPerformed(ActionEvent e) {
-//               dumpStuff(new Throwable());
-//            }
-//        };
-
-//      if (!keyMap.isKeyStrokeDefined("special dump")) {
-//         ks = KeyStroke.getKeyStroke(KeyEvent.VK_O,KeyEvent.CTRL_MASK | KeyEvent.SHIFT_MASK);
-//      }
-//      else {
-//         ks = keyMap.getKeyStroke(MNEMONIC_RUN_SCRIPT);
-//      }
-//      getInputMap().put(ks,"special dump");
-//      getActionMap().put("special dump",spclDump );
+      new NewSessionAction(session,keyMap);
+      new ToggleConnectionAction(session,keyMap);
+      new JumpNextAction(session,keyMap);
+      new JumpPrevAction(session,keyMap);
+      new HotspotsAction(session,keyMap);
+      new GuiAction(session,keyMap);
+      new DispMsgsAction(session,keyMap);
+      new AttributesAction(session,keyMap);
+      new PrintAction(session,keyMap);
+      new RulerAction(session,keyMap);
+      new CloseAction(session,keyMap);
+      new TransferAction(session,keyMap);
+      new EmailAction(session,keyMap);
+      new RunScriptAction(session,keyMap);
+      new DebugAction(session,keyMap);
 
    }
 
@@ -419,67 +182,6 @@ public class DefaultKeyboardHandler extends KeyboardHandler {
 
    private void processVTKeyPressed(KeyEvent e){
 
-//      keyProcessed = true;
-//      displayInfo(e,"Pressed " + keyProcessed);
-//      int keyCode = e.getKeyCode();
-//
-//      if (isLinux && keyCode == e.VK_ALT_GRAPH) {
-//
-//         isAltGr = true;
-//      }
-//
-//      if (      keyCode == e.VK_CAPS_LOCK ||
-//            keyCode == e.VK_SHIFT ||
-//            keyCode == e.VK_ALT ||
-//            keyCode == e.VK_ALT_GRAPH
-//         ) {
-//         displayInfo(e,"Pressed ");
-//
-//         return;
-//      }
-//
-//      displayInfo(e,"Pressed " + keyProcessed);
-//
-//      KeyStroke ks = KeyStroke.getKeyStroke(e.getKeyCode(),e.getModifiers(),false);
-//
-//      if (emulatorAction(ks,e)) {
-//
-//         return;
-//      }
-//
-//      if (isLinux)
-//         lastKeyStroke = keyMap.getKeyStrokeText(e,isAltGr);
-//      else
-//         lastKeyStroke = keyMap.getKeyStrokeText(e);
-//
-//      //System.out.println("lastKeyStroke " + lastKeyStroke);
-//
-//      if (lastKeyStroke != null && !lastKeyStroke.equals("null")) {
-//
-//         if (lastKeyStroke.startsWith("[") || lastKeyStroke.length() == 1) {
-//
-//            screen.sendKeys(lastKeyStroke);
-//            if (recording)
-//               recordBuffer.append(lastKeyStroke);
-//         }
-//         else {
-//            session.executeMeMacro(lastKeyStroke);
-//         }
-//         if (lastKeyStroke.startsWith("[mark")) {
-//            if (lastKeyStroke.equals("[markleft]") ||
-//                  lastKeyStroke.equals("[markright]") ||
-//                  lastKeyStroke.equals("[markup]") ||
-//                  lastKeyStroke.equals("[markdown]")) {
-//               session.doKeyBoundArea(e,lastKeyStroke);
-//            }
-//         }
-//      }
-//      else
-//         keyProcessed = false;
-//
-//      if (keyProcessed)
-//         e.consume();
-//
 
       keyProcessed = true;
 //      displayInfo(e,"Pressed " + keyProcessed);
