5d726357f0ee31b46687c055f4e2f1a94722033f
Patrick Bielen <pbielen@users.sourceforge.net>
2003-03-03 12:21:30 +0000

    Removed the hide_on_close statement, cause that's default behaviour.

--- scripts/LogOn.py
+++ scripts/LogOn.py
@@ -3,23 +3,22 @@ import java.awt as awt
 
 class LogOn(swing.JDialog):
     def __init__(self, parent=None):
-	swing.JDialog.__init__(self, title="AS/400 - LogIn", modal=1)
-	self.setDefaultCloseOperation(swing.WindowConstants.HIDE_ON_CLOSE)
+        swing.JDialog.__init__(self, title="AS/400 - LogIn", modal=1)
 	self.setResizable(0)
 	self.setDefaultLookAndFeelDecorated(1)
 	self.setLocationRelativeTo(self)
 	self.contentPane.setLayout(awt.GridBagLayout())
 	gbc = awt.GridBagConstraints()
-		
+	
 	lbl1 = swing.JLabel("Server", preferredSize=(60, 22))
-	lbl2 = swing.JLabel("Name", preferredSize=(60, 22))
-	lbl3 = swing.JLabel("Password", preferredSize=(60, 22))
+	lbl2 = swing.JLabel("Naam", preferredSize=(60, 22))
+	lbl3 = swing.JLabel("Paswoord", preferredSize=(60, 22))
 	self.txt1 = swing.JTextField(preferredSize=(125, 22))
 	self.txt2 = swing.JTextField(preferredSize=(125, 22))
 	self.txt3 = swing.JTextField(preferredSize=(125, 22))
 	btn = swing.JButton("OK", preferredSize=(75, 25), actionPerformed=self.sendData)
 	btn.setMnemonic('O')
-		
+	
 	gbc.insets = awt.Insets(15, 10, 15, 5)
 	self.contentPane.add(lbl1, gbc)
 	gbc.insets = awt.Insets(15, 5, 15, 10)
@@ -41,10 +40,8 @@ class LogOn(swing.JDialog):
 	gbc.insets = awt.Insets(10, 10, 20, 10)
 	self.contentPane.add(btn, gbc)
 	self.pack()
-		
     def sendData(self, event):
-	self.hide()
-
+           self.hide()
 if __name__ == "__main__":
     dlg = LogOn()
     dlg.show()
