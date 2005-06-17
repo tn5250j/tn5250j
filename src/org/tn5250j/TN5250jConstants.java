4112166856accebda9baef17d32b4ec886518b53
Kenneth J. Pouncey <kjp1@users.sourceforge.net>
2005-06-17 04:35:55 +0000

    Getting ready for release

--- src/org/tn5250j/TN5250jConstants.java
+++ src/org/tn5250j/TN5250jConstants.java
@@ -25,8 +25,8 @@ public interface TN5250jConstants {
 
    // Version information
    public static final String tn5250jRelease = "0";
-   public static final String tn5250jVersion = ".5";
-   public static final String tn5250jSubVer= ".8 alpha 5";
+   public static final String tn5250jVersion = ".6";
+   public static final String tn5250jSubVer= ".0";
 
    // STATE
    static final int STATE_DISCONNECTED   =  0;
