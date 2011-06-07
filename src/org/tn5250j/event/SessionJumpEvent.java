73c46fd97ca3e14215d86441f300916eec6d9d9c
Martin W. Kirst <maki@bitkings.de>
2011-06-07 21:19:13 +0000

    re-indent

--- src/org/tn5250j/event/SessionJumpEvent.java
+++ src/org/tn5250j/event/SessionJumpEvent.java
@@ -4,34 +4,35 @@ import java.util.EventObject;
 
 public class SessionJumpEvent extends EventObject {
 
-   private static final long serialVersionUID = 1L;
-public SessionJumpEvent(Object obj){
-      super(obj);
-   }
+	private static final long serialVersionUID = 1L;
+	
+	public SessionJumpEvent(Object obj){
+		super(obj);
+	}
 
-   public SessionJumpEvent(Object obj, String s) {
-      super(obj);
-      message = s;
-   }
+	public SessionJumpEvent(Object obj, String s) {
+		super(obj);
+		message = s;
+	}
 
-   public String getMessage() {
-      return message;
-   }
+	public String getMessage() {
+		return message;
+	}
 
-   public void setMessage(String s) {
-      message = s;
-   }
+	public void setMessage(String s) {
+		message = s;
+	}
 
-   public int getJumpDirection() {
+	public int getJumpDirection() {
 
-      return dir;
-   }
+		return dir;
+	}
 
-   public void setJumpDirection(int d) {
+	public void setJumpDirection(int d) {
 
-      dir = d;
-   }
+		dir = d;
+	}
 
-   private String message;
-   private int dir;
+	private String message;
+	private int dir;
 }
