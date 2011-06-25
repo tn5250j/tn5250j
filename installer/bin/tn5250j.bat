af87645451271c69cd0a7edcc48515faf94e3c41
Martin W. Kirst <maki@bitkings.de>
2011-06-25 06:59:49 +0000

    updated IzPack installation, thanks sgonzalez3884

--- installer/bin/tn5250j.bat
+++ installer/bin/tn5250j.bat
@@ -3,7 +3,9 @@
 @rem DOS Batch file to invoke the emulator
 @rem ======================================
 
-SET EMUL_PATH=$INSTALL_PATH
-java -jar "%EMUL_PATH%\lib\tn5250j.jar"
+setlocal
 
-@echo on
+SET EMUL_PATH=$INSTALL_PATH
+start javaw -jar "%EMUL_PATH%\lib\tn5250j.jar"
+
+endlocal
