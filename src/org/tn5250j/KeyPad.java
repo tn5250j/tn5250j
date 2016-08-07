66bf65d657140a82042bb94bba3b4e737c9c4d1e
nitram509 <maki@bitkings.de>
2016-08-07 23:10:57 +0200

    make methods private remove comments order fields on top

--- src/org/tn5250j/KeyPad.java
+++ src/org/tn5250j/KeyPad.java
@@ -1,5 +1,5 @@
 package org.tn5250j;
-/**
+/*
  * Title: tn5250J
  * Copyright:   Copyright (c) 2001
  * Company:
@@ -25,25 +25,28 @@ package org.tn5250j;
  * Boston, MA 02111-1307 USA
  */
 
-import org.tn5250j.tools.LangTool;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 import static org.tn5250j.TN5250jConstants.*;
+import static org.tn5250j.tools.LangTool.getString;
 
 public class KeyPad extends JPanel {
 
     private static final long serialVersionUID = -7460283401326716314L;
 
-    BorderLayout borderLayout1 = new BorderLayout();
-    JButton[] buttons = new JButton[30];
-    int bSize = 0;
-    JPanel keyPadTop;
-    JPanel keyPadBottom;
-    GridLayout gridLayout1 = new GridLayout();
-    int numPad = 1;
+    private static Rectangle textRect = new Rectangle();
+    private static Rectangle iconRect = new Rectangle();
+    private static int MIN_SIZE = 3;
+
+    private BorderLayout borderLayout1 = new BorderLayout();
+    private JButton[] buttons = new JButton[30];
+    private int bSize = 0;
+    private JPanel keyPadTop;
+    private JPanel keyPadBottom;
+    private GridLayout gridLayout1 = new GridLayout();
+    private int numPad = 1;
 
     public KeyPad() {
         try {
@@ -98,217 +101,135 @@ public class KeyPad extends JPanel {
 
         switch (which) {
 
-//         case 1:
-//            buttons[0].setLabel("PF1 ");
-//            buttons[0].setActionCommand("PF1");
-//            buttons[1].setLabel("PF2 ");
-//            buttons[1].setActionCommand("PF2");
-//            buttons[2].setLabel("PF3 ");
-//            buttons[2].setActionCommand("PF3");
-//            buttons[3].setLabel("PF4 ");
-//            buttons[3].setActionCommand("PF4");
-//            buttons[4].setLabel("PF5 ");
-//            buttons[4].setActionCommand("PF5");
-//            buttons[5].setLabel("PF6 ");
-//            buttons[5].setActionCommand("PF6");
-//            buttons[6].setLabel("Enter");
-//            buttons[6].setActionCommand("ENTER");
-//            buttons[7].setLabel("PgUp");
-//            buttons[7].setActionCommand("PGUP");
-//            buttons[8].setLabel("Clear");
-//            buttons[8].setActionCommand("CLEAR");
-//
-//            buttons[9].setLabel("PF7 ");
-//            buttons[9].setActionCommand("PF7");
-//            buttons[10].setLabel("PF8 ");
-//            buttons[10].setActionCommand("PF8");
-//            buttons[11].setLabel("PF9 ");
-//            buttons[11].setActionCommand("PF9");
-//            buttons[12].setLabel("PF10");
-//            buttons[12].setActionCommand("PF10");
-//            buttons[13].setLabel("PF11");
-//            buttons[13].setActionCommand("PF11");
-//            buttons[14].setLabel("PF12");
-//            buttons[14].setActionCommand("PF12");
-//            buttons[15].setLabel("SyRq");
-//            buttons[15].setActionCommand("SR");
-//            buttons[16].setLabel("PgDn");
-//            buttons[16].setActionCommand("PGDN");
-//
-//            break;
-//         case 2:
-//
-//            buttons[0].setLabel("PF13");
-//            buttons[0].setActionCommand("PF13");
-//            buttons[1].setLabel("PF14");
-//            buttons[1].setActionCommand("PF14");
-//            buttons[2].setLabel("PF15");
-//            buttons[2].setActionCommand("PF15");
-//            buttons[3].setLabel("PF16");
-//            buttons[3].setActionCommand("PF16");
-//            buttons[4].setLabel("PF17");
-//            buttons[4].setActionCommand("PF17");
-//            buttons[5].setLabel("PF18");
-//            buttons[5].setActionCommand("PF18");
-//            buttons[6].setLabel("Enter");
-//            buttons[6].setActionCommand("ENTER");
-//            buttons[7].setLabel("Pg Up");
-//            buttons[7].setActionCommand("PGUP");
-//            buttons[8].setLabel("Help");
-//            buttons[8].setActionCommand("HELP");
-//
-//
-//            buttons[9].setLabel("PF19");
-//            buttons[9].setActionCommand("PF19");
-//            buttons[10].setLabel("PF20");
-//            buttons[10].setActionCommand("PF20");
-//            buttons[11].setLabel("PF21");
-//            buttons[11].setActionCommand("PF21");
-//            buttons[12].setLabel("PF22");
-//            buttons[12].setActionCommand("PF22");
-//            buttons[13].setLabel("PF23");
-//            buttons[13].setActionCommand("PF23");
-//            buttons[14].setLabel("PF24");
-//            buttons[14].setActionCommand("PF24");
-//            buttons[15].setLabel("SyRq");
-//            buttons[15].setActionCommand("SR");
-//            buttons[16].setLabel("PgDn");
-//            buttons[16].setActionCommand("PGDN");
-//
-//            break;
-//      }
-//      buttons[17].setLabel("Next Pad");
-//      buttons[17].setActionCommand("NXTPAD");
-
             case 1:
-                buttons[0].setText(LangTool.getString("KP_F1", "PF1"));
+                buttons[0].setText(getString("KP_F1", "PF1"));
                 buttons[0].setActionCommand(MNEMONIC_PF1);
-                buttons[1].setText(LangTool.getString("KP_F2", "PF2"));
+                buttons[1].setText(getString("KP_F2", "PF2"));
                 buttons[1].setActionCommand(MNEMONIC_PF2);
-                buttons[2].setText(LangTool.getString("KP_F3", "PF3"));
+                buttons[2].setText(getString("KP_F3", "PF3"));
                 buttons[2].setActionCommand(MNEMONIC_PF3);
-                buttons[3].setText(LangTool.getString("KP_F4", "PF4"));
+                buttons[3].setText(getString("KP_F4", "PF4"));
                 buttons[3].setActionCommand(MNEMONIC_PF4);
-                buttons[4].setText(LangTool.getString("KP_F5", "PF5"));
+                buttons[4].setText(getString("KP_F5", "PF5"));
                 buttons[4].setActionCommand(MNEMONIC_PF5);
-                buttons[5].setText(LangTool.getString("KP_F6", "PF6"));
+                buttons[5].setText(getString("KP_F6", "PF6"));
                 buttons[5].setActionCommand(MNEMONIC_PF6);
-                buttons[6].setText(LangTool.getString("KP_F7", "PF7"));
+                buttons[6].setText(getString("KP_F7", "PF7"));
                 buttons[6].setActionCommand(MNEMONIC_PF7);
-                buttons[7].setText(LangTool.getString("KP_F8", "PF8"));
+                buttons[7].setText(getString("KP_F8", "PF8"));
                 buttons[7].setActionCommand(MNEMONIC_PF8);
-                buttons[8].setText(LangTool.getString("KP_F9", "PF9"));
+                buttons[8].setText(getString("KP_F9", "PF9"));
                 buttons[8].setActionCommand(MNEMONIC_PF9);
-                buttons[9].setText(LangTool.getString("KP_F10", "PF10"));
+                buttons[9].setText(getString("KP_F10", "PF10"));
                 buttons[9].setActionCommand(MNEMONIC_PF10);
-                buttons[10].setText(LangTool.getString("KP_F11", "PF11"));
+                buttons[10].setText(getString("KP_F11", "PF11"));
                 buttons[10].setActionCommand(MNEMONIC_PF11);
-                buttons[11].setText(LangTool.getString("KP_F12", "PF12"));
+                buttons[11].setText(getString("KP_F12", "PF12"));
                 buttons[11].setActionCommand(MNEMONIC_PF12);
-                buttons[12].setText(LangTool.getString("KP_ENTER", "Enter"));
+                buttons[12].setText(getString("KP_ENTER", "Enter"));
                 buttons[12].setActionCommand(MNEMONIC_ENTER);
-                buttons[13].setText(LangTool.getString("KP_PGUP", "PgUp"));
+                buttons[13].setText(getString("KP_PGUP", "PgUp"));
                 buttons[13].setActionCommand(MNEMONIC_PAGE_UP);
-                buttons[14].setText(LangTool.getString("KP_CLEAR", "Clear"));
+                buttons[14].setText(getString("KP_CLEAR", "Clear"));
                 buttons[14].setActionCommand(MNEMONIC_CLEAR);
 
-                buttons[15].setText(LangTool.getString("KP_F13", "PF13"));
+                buttons[15].setText(getString("KP_F13", "PF13"));
                 buttons[15].setActionCommand(MNEMONIC_PF13);
-                buttons[16].setText(LangTool.getString("KP_F14", "PF14"));
+                buttons[16].setText(getString("KP_F14", "PF14"));
                 buttons[16].setActionCommand(MNEMONIC_PF14);
-                buttons[17].setText(LangTool.getString("KP_F15", "PF15"));
+                buttons[17].setText(getString("KP_F15", "PF15"));
                 buttons[17].setActionCommand(MNEMONIC_PF15);
-                buttons[18].setText(LangTool.getString("KP_F16", "PF16"));
+                buttons[18].setText(getString("KP_F16", "PF16"));
                 buttons[18].setActionCommand(MNEMONIC_PF16);
-                buttons[19].setText(LangTool.getString("KP_F17", "PF17"));
+                buttons[19].setText(getString("KP_F17", "PF17"));
                 buttons[19].setActionCommand(MNEMONIC_PF17);
-                buttons[20].setText(LangTool.getString("KP_F18", "PF18"));
+                buttons[20].setText(getString("KP_F18", "PF18"));
                 buttons[20].setActionCommand(MNEMONIC_PF18);
-                buttons[21].setText(LangTool.getString("KP_F19", "PF19"));
+                buttons[21].setText(getString("KP_F19", "PF19"));
                 buttons[21].setActionCommand(MNEMONIC_PF19);
-                buttons[22].setText(LangTool.getString("KP_F20", "PF20"));
+                buttons[22].setText(getString("KP_F20", "PF20"));
                 buttons[22].setActionCommand(MNEMONIC_PF20);
-                buttons[23].setText(LangTool.getString("KP_F21", "PF21"));
+                buttons[23].setText(getString("KP_F21", "PF21"));
                 buttons[23].setActionCommand(MNEMONIC_PF21);
-                buttons[24].setText(LangTool.getString("KP_F22", "PF22"));
+                buttons[24].setText(getString("KP_F22", "PF22"));
                 buttons[24].setActionCommand(MNEMONIC_PF22);
-                buttons[25].setText(LangTool.getString("KP_F23", "PF23"));
+                buttons[25].setText(getString("KP_F23", "PF23"));
                 buttons[25].setActionCommand(MNEMONIC_PF23);
-                buttons[26].setText(LangTool.getString("KP_F24", "PF24"));
+                buttons[26].setText(getString("KP_F24", "PF24"));
                 buttons[26].setActionCommand(MNEMONIC_PF24);
-                buttons[27].setText(LangTool.getString("KP_SR", "SysReq"));
+                buttons[27].setText(getString("KP_SR", "SysReq"));
                 buttons[27].setActionCommand(MNEMONIC_SYSREQ);
-                buttons[28].setText(LangTool.getString("KP_PGDN", "PgDn"));
+                buttons[28].setText(getString("KP_PGDN", "PgDn"));
                 buttons[28].setActionCommand(MNEMONIC_PAGE_DOWN);
 
                 break;
 
             case 2:
 
-                buttons[0].setText(LangTool.getString("KP_F1", "PF1"));
+                buttons[0].setText(getString("KP_F1", "PF1"));
                 buttons[0].setActionCommand(MNEMONIC_PF1);
-                buttons[1].setText(LangTool.getString("KP_F2", "PF2"));
+                buttons[1].setText(getString("KP_F2", "PF2"));
                 buttons[1].setActionCommand(MNEMONIC_PF2);
-                buttons[2].setText(LangTool.getString("KP_F3", "PF3"));
+                buttons[2].setText(getString("KP_F3", "PF3"));
                 buttons[2].setActionCommand(MNEMONIC_PF3);
-                buttons[3].setText(LangTool.getString("KP_F4", "PF4"));
+                buttons[3].setText(getString("KP_F4", "PF4"));
                 buttons[3].setActionCommand(MNEMONIC_PF4);
-                buttons[4].setText(LangTool.getString("KP_F5", "PF5"));
+                buttons[4].setText(getString("KP_F5", "PF5"));
                 buttons[4].setActionCommand(MNEMONIC_PF5);
-                buttons[5].setText(LangTool.getString("KP_F6", "PF6"));
+                buttons[5].setText(getString("KP_F6", "PF6"));
                 buttons[5].setActionCommand(MNEMONIC_PF6);
-                buttons[6].setText(LangTool.getString("KP_F7", "PF7"));
+                buttons[6].setText(getString("KP_F7", "PF7"));
                 buttons[6].setActionCommand(MNEMONIC_PF7);
-                buttons[7].setText(LangTool.getString("KP_F8", "PF8"));
+                buttons[7].setText(getString("KP_F8", "PF8"));
                 buttons[7].setActionCommand(MNEMONIC_PF8);
-                buttons[8].setText(LangTool.getString("KP_F9", "PF9"));
+                buttons[8].setText(getString("KP_F9", "PF9"));
                 buttons[8].setActionCommand(MNEMONIC_PF9);
-                buttons[9].setText(LangTool.getString("KP_F10", "PF10"));
+                buttons[9].setText(getString("KP_F10", "PF10"));
                 buttons[9].setActionCommand(MNEMONIC_PF10);
-                buttons[10].setText(LangTool.getString("KP_F11", "PF11"));
+                buttons[10].setText(getString("KP_F11", "PF11"));
                 buttons[10].setActionCommand(MNEMONIC_PF11);
-                buttons[11].setText(LangTool.getString("KP_F12", "PF12"));
+                buttons[11].setText(getString("KP_F12", "PF12"));
                 buttons[11].setActionCommand(MNEMONIC_PF12);
-                buttons[12].setText(LangTool.getString("KP_ENTER", "Enter"));
+                buttons[12].setText(getString("KP_ENTER", "Enter"));
                 buttons[12].setActionCommand(MNEMONIC_ENTER);
-                buttons[13].setText(LangTool.getString("KP_PGUP", "PgUp"));
+                buttons[13].setText(getString("KP_PGUP", "PgUp"));
                 buttons[13].setActionCommand(MNEMONIC_PAGE_UP);
-                buttons[14].setText(LangTool.getString("KP_HELP", "Help"));
+                buttons[14].setText(getString("KP_HELP", "Help"));
                 buttons[14].setActionCommand(MNEMONIC_HELP);
 
-                buttons[15].setText(LangTool.getString("KP_F13", "PF13"));
+                buttons[15].setText(getString("KP_F13", "PF13"));
                 buttons[15].setActionCommand(MNEMONIC_PF13);
-                buttons[16].setText(LangTool.getString("KP_F14", "PF14"));
+                buttons[16].setText(getString("KP_F14", "PF14"));
                 buttons[16].setActionCommand(MNEMONIC_PF14);
-                buttons[17].setText(LangTool.getString("KP_F15", "PF15"));
+                buttons[17].setText(getString("KP_F15", "PF15"));
                 buttons[17].setActionCommand(MNEMONIC_PF15);
-                buttons[18].setText(LangTool.getString("KP_F16", "PF16"));
+                buttons[18].setText(getString("KP_F16", "PF16"));
                 buttons[18].setActionCommand(MNEMONIC_PF16);
-                buttons[19].setText(LangTool.getString("KP_F17", "PF17"));
+                buttons[19].setText(getString("KP_F17", "PF17"));
                 buttons[19].setActionCommand(MNEMONIC_PF17);
-                buttons[20].setText(LangTool.getString("KP_F18", "PF18"));
+                buttons[20].setText(getString("KP_F18", "PF18"));
                 buttons[20].setActionCommand(MNEMONIC_PF18);
-                buttons[21].setText(LangTool.getString("KP_F19", "PF19"));
+                buttons[21].setText(getString("KP_F19", "PF19"));
                 buttons[21].setActionCommand(MNEMONIC_PF19);
-                buttons[22].setText(LangTool.getString("KP_F20", "PF20"));
+                buttons[22].setText(getString("KP_F20", "PF20"));
                 buttons[22].setActionCommand(MNEMONIC_PF20);
-                buttons[23].setText(LangTool.getString("KP_F21", "PF21"));
+                buttons[23].setText(getString("KP_F21", "PF21"));
                 buttons[23].setActionCommand(MNEMONIC_PF21);
-                buttons[24].setText(LangTool.getString("KP_F22", "PF22"));
+                buttons[24].setText(getString("KP_F22", "PF22"));
                 buttons[24].setActionCommand(MNEMONIC_PF22);
-                buttons[25].setText(LangTool.getString("KP_F23", "PF23"));
+                buttons[25].setText(getString("KP_F23", "PF23"));
                 buttons[25].setActionCommand(MNEMONIC_PF23);
-                buttons[26].setText(LangTool.getString("KP_F24", "PF24"));
+                buttons[26].setText(getString("KP_F24", "PF24"));
                 buttons[26].setActionCommand(MNEMONIC_PF24);
-                buttons[27].setText(LangTool.getString("KP_SR", "SysReq"));
+                buttons[27].setText(getString("KP_SR", "SysReq"));
                 buttons[27].setActionCommand(MNEMONIC_SYSREQ);
-                buttons[28].setText(LangTool.getString("KP_PGDN", "PgDn"));
-                buttons[28].setActionCommand(TN5250jConstants.MNEMONIC_PAGE_DOWN);
+                buttons[28].setText(getString("KP_PGDN", "PgDn"));
+                buttons[28].setActionCommand(MNEMONIC_PAGE_DOWN);
 
                 break;
         }
 
-        buttons[29].setText(LangTool.getString("KP_NXTPAD", "Next Pad"));
+        buttons[29].setText(getString("KP_NXTPAD", "Next Pad"));
         buttons[29].setActionCommand("NXTPAD");
 
     }
@@ -330,18 +251,7 @@ public class KeyPad extends JPanel {
 
     }
 
-    /* These rectangles/insets are allocated once for all
-      * calls.  Re-using rectangles rather than allocating them in each paint
-      * call substantially reduced the time it took paint to run.
-      */
-    private static Rectangle viewRect = new Rectangle();
-    private static Rectangle textRect = new Rectangle();
-    private static Rectangle iconRect = new Rectangle();
-    private static String endsWithSuffix = "...";
-    private static int minSize = 3;
 
-    /**
-     */
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
@@ -352,7 +262,7 @@ public class KeyPad extends JPanel {
         FontMetrics fm = buttons[0].getFontMetrics(k);
 
         // initializ the viewRect which is the visible rectangle
-        viewRect = buttons[0].getVisibleRect();
+        Rectangle viewRect = buttons[0].getVisibleRect();
 
         Insets i = buttons[0].getInsets();
 
@@ -376,14 +286,14 @@ public class KeyPad extends JPanel {
                 viewRect,
                 textRect,
                 iconRect,
-                0).endsWith(endsWithSuffix)
-                && fs > (minSize - 1)) {
+                0).endsWith("...")
+                && fs > (MIN_SIZE - 1)) {
             k = k.deriveFont(--fs);
             fm = buttons[0].getFontMetrics(k);
 
         }
 
-        if (fs >= minSize) {
+        if (fs >= MIN_SIZE) {
 
             for (int x = 0; x < bSize; x++) {
                 buttons[x].setFont(k);
