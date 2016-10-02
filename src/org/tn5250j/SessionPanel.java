bc765181936c79766a5347e5334fec3d1e3f9dca
nitram509 <maki@bitkings.de>
2016-10-02 12:20:46 +0200

    remove useless comments boyscouting: prefer enums of string literals

--- src/org/tn5250j/SessionPanel.java
+++ src/org/tn5250j/SessionPanel.java
@@ -53,6 +53,7 @@ import org.tn5250j.framework.tn5250.Screen5250;
 import org.tn5250j.framework.tn5250.tnvt;
 import org.tn5250j.gui.ConfirmTabCloseDialog;
 import org.tn5250j.keyboard.KeyboardHandler;
+import org.tn5250j.keyboard.KeypadMnemonic;
 import org.tn5250j.keyboard.KeypadMnemonicSerializer;
 import org.tn5250j.mailtools.SendEMailDialog;
 import org.tn5250j.sessionsettings.SessionSettings;
@@ -64,6 +65,7 @@ import org.tn5250j.tools.logging.TN5250jLogger;
 import static org.tn5250j.SessionConfig.CONFIG_KEYPAD_ENABLED;
 import static org.tn5250j.SessionConfig.CONFIG_KEYPAD_MNEMONICS;
 import static org.tn5250j.SessionConfig.YES;
+import static org.tn5250j.keyboard.KeypadMnemonic.ENTER;
 
 /**
  * A host GUI session
@@ -170,8 +172,7 @@ public class SessionPanel extends JPanel implements RubberBandCanvasIF, SessionC
 				}
 
 				if (e.getClickCount() == 2 & doubleClick) {
-
-					screen.sendKeys("[enter]");
+					screen.sendKeys(ENTER);
 				}
 				else {
 					int pos = guiGraBuf.getPosFromView(e.getX(), e.getY());
